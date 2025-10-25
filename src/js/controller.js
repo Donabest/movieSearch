c2c07cb5b2541636e2caaf19e1b1a36829dd60d8
Donabest <rabiuazeez3@gmail.com>
2025-10-25 18:46:33 +0100

    feat:render the movie when click on suggestion

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,8 +4,10 @@ import { Navigation } from 'swiper/modules';
 import '/node_modules/swiper/swiper-bundle.min.css';
 
 const API_KEY = 'a88569285faf43dba08ff3d8ebf5ce71';
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const POST_URL = 'https://image.tmdb.org/t/p/w500';
+const IMG_URL = 'https://image.tmdb.org/t/p/original/';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const YOUTUBE_URL = 'https://www.youtube.com/watch?v=';
 
 const swipperWrapper = document.querySelector('.swiper-wrapper');
 const hamburgerMenu = document.querySelector('.fa-bars');
@@ -18,6 +20,9 @@ const bookmarkDisplay = document.querySelector('.bookmark-overlay');
 const modal = document.querySelector('.modal');
 const modalDetails = document.querySelector('.modal--detail');
 const inputWrapper = document.querySelector('.search-input-wrapper');
+const sectionContent = document.querySelector('.section-content');
+const suggestionList = document.querySelector('.suggestion-list');
+const inputValue = document.querySelector('.search-input');
 
 const runtime = function (runtime) {
   const hour = Math.floor(runtime / 60);
@@ -93,11 +98,11 @@ async function getMovieData(query = 'batman') {
   }
 }
 
-async function getMovieDetails(id) {
+async function getMovieDetails(id, type = 'movie') {
   try {
     const [res1, res2] = await Promise.all([
       fetch(`${BASE_URL}/movie/${id}/videos?api_key=${API_KEY}`),
-      fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`),
+      fetch(`${BASE_URL}/${type}/${id}?api_key=${API_KEY}`),
     ]);
     const [data1, data2] = await Promise.all([res1.json(), res2.json()]);
     return [data1, data2];
@@ -105,23 +110,74 @@ async function getMovieDetails(id) {
     console.error(err);
   }
 }
-
-inputWrapper.addEventListener('input', function (e) {
-  e.preventDefault();
-  const inputValue = e.target.value.trim().toLowerCase();
-  if (inputValue == ' ') return;
-  getMovieData(inputValue).then(data => {
-    const results = data[1].results.splice(0, 5);
-    const suggestionList = document.querySelector('.suggestion-list');
-    suggestionList.innerHTML = results
-      .map(suggestion => {
-        return `<li data-sugges="${suggestion.id}">${
-          suggestion.title || suggestion.name
-        }</li>`;
-      })
-      .join('');
+function movie() {
+  inputWrapper.addEventListener('input', function (e) {
+    e.preventDefault();
+    const inputValue = e.target.value.trim().toLowerCase();
+    if (inputValue == ' ') return;
+    getMovieData(inputValue).then(data => {
+      const results = data[1].results.splice(0, 5);
+      suggestionList.innerHTML = results
+        .map(suggestion => {
+          return `<li data-sugges="${suggestion.id}" data-type="${
+            suggestion.media_type
+          }">${suggestion.title || suggestion.name}</li>`;
+        })
+        .join('');
+    });
   });
-});
+
+  suggestionList.addEventListener('click', e => {
+    const target = e.target.dataset.sugges;
+    const type = e.target.dataset.type;
+    getMovieDetails(target, type).then(details => {
+      const detail1 = details[1];
+      const detail0 = details[0];
+      const header = document.getElementById('main--header');
+      header.style.background = `linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.9) 0,
+      rgba(0, 0, 0, 0.4) 60%,
+      rgba(0, 0, 0, 0.95) 100%
+    ),url('${
+      detail1.backdrop_path
+        ? IMG_URL + detail1.backdrop_path
+        : POST_URL + detail1.profile_path
+    }')`;
+      header.style.backgroundPosition = 'center';
+      header.style.backgroundRepeat = 'no-repeat';
+      header.style.backgroundSize = 'cover';
+
+      sectionContent.innerHTML = `
+          <h2>${detail1.title || detail1.name}</h2>
+          <p>${detail1.overview || detail1.biography}</p>
+          <div class="section-btn">
+            <button class="btn btn--watch ${
+              detail0.success === false ? ' ' : 'show'
+            } ">
+              <a href="${
+                detail0.success === false
+                  ? ''
+                  : YOUTUBE_URL + detail0?.result0?.key
+              }" target="_blank">Watch Now</a
+              ><i class="fa-solid fa-play"></i>
+            </button>
+            <button class="btn btn-details">
+              Details <i class="fa-solid fa-angle-right"></i>
+            </button>
+          </div>`;
+      searchInputtoggle();
+      inputValue.value = '';
+      suggestionList.innerHTML = '';
+    });
+  });
+  inputWrapper.addEventListener('submit', e => {
+    e.preventDefault();
+    const value = inputValue.value.trim().toLowerCase();
+    if (!value) return;
+  });
+}
+movie();
 
 async function loadTrendingData() {
   try {
@@ -132,9 +188,9 @@ async function loadTrendingData() {
         .map(trending => {
           return `
             <div class="swiper-slide" >
-              <img src="${IMG_URL + trending.poster_path}" alt="" data-movie='${
-            trending.id
-          }' />
+              <img src="${
+                POST_URL + trending.poster_path
+              }" alt="" data-movie='${trending.id}' />
             </div>
           `;
         })
@@ -156,7 +212,7 @@ async function loadTrendingData() {
               const html = `
                 <span class="modal-hidden"><i class="fa-solid fa-xmark"></i> </span>
                 <div class="movie--bg">
-                 <img src="${IMG_URL + movie.poster_path}" alt="" />
+                 <img src="${POST_URL + movie.poster_path}" alt="" />
                 </div>
                 <h4>${movie.title}</h4>
                 <div class="modal--catagories">
@@ -170,17 +226,17 @@ async function loadTrendingData() {
                   }</span>
                   <span class="movie--categ">${movieGenre.genres[0].name}</span>
                   <span class="movie--genre">${
-                    movieGenre.genres[1]?.name
+                    movieGenre.genres[1]
                       ? movieGenre.genres[1].name
                       : movieGenre.genres[0].name
                   }</span>
                 </div>
                 <div class="modal--overview">
-                  <p>${movie.overview}</p>
+                  <p>${movie.overview || movie.biography}</p>
                 </div>
                 <button class="modal--btn-trailer">
-                 <i class="fa-solid fa-play"></i> <a href="https://www.youtube.com/watch?v=${
-                   movieTrailerData.results[0].key
+                 <i class="fa-solid fa-play"></i> <a href="${
+                   YOUTUBE_URL + movieTrailerData.results[0].key
                  }" target="_blank">play Trailer</a>
                 </button>
                 <button class="modal--icon">
